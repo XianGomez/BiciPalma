c48e9b8705d6b2720bb4ef7b8da56e07816bb1b3
XianGomez <xiiangomezz@gmail.com>
2026-04-10 14:08:45 +0200

    feat(Estacion): Añadir funcion anclajesLibres

--- src/main/java/edu/teamrocket/bicipalma/estacion/Estacion.java
+++ src/main/java/edu/teamrocket/bicipalma/estacion/Estacion.java
@@ -1,23 +1,30 @@
 package edu.teamrocket.bicipalma.estacion;
 
+import edu.teamrocket.bicipalma.bicicleta.Bicicleta;
+
 public class Estacion {
     
     private int id;
     private String direccion;
-    private int numAnclajes;
+    private Bicicleta[] anclajes; 
      
-    
     public Estacion(int id, String direccion, int numAnclajes) {
         this.id = id;
         this.direccion = direccion;
-        this.numAnclajes= numAnclajes;
+        this.anclajes = new Bicicleta[numAnclajes];
 
     }
 
     public void consultarEstacion() {
-        System.out.println("id: " + this.id + ", direccion: " + this.direccion + ", anclaje: " + this.numAnclajes);
+        System.out.println("id: " + this.id + ", direccion: " + this.direccion + ", anclaje: " + this.anclajes.length);
     }
 
-    
+    public int anclajesLibres() {
+        int libres = anclajes.length;
+        for (Bicicleta b: anclajes) {
+            if (b != null) libres--;
+        }
+        return libres;
+    }
 
 }
