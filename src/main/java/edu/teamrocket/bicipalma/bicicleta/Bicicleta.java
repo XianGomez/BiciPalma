6de79ac0fa1b6e1a7600a78bb7d50d456d10a2b1
XianGomez <xiiangomezz@gmail.com>
2026-04-13 12:11:56 +0200

    feat(Bicicleta): Crear clase bicicleta con constructor

---
+++ src/main/java/edu/teamrocket/bicipalma/bicicleta/Bicicleta.java
@@ -0,0 +1,14 @@
+package edu.teamrocket.bicipalma.bicicleta;
+
+public class Bicicleta {
+
+    private int id;
+
+    public Bicicleta(int id) {
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
+}
